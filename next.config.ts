7d3636760a0a45eaf6d06f8957f80732d8084227
jeyamuruganrani <jeya.muruagn@swarise.com>
2025-08-23 14:30:11 +0530

    allow the Swarise images

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["swarise.com"], 
+  },
 };
 
 export default nextConfig;
